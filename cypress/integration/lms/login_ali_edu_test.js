f41b46979fcc12f96914edfbae60e4133b7e72c0
Lee Dong Wook <sh95119@gmail.com>
2021-02-12 04:15:52 +0900

    Update and rename login_test.js to login_ali_edu_test.js

--- cypress/integration/lms/login_ali_edu_test.js
+++ cypress/integration/lms/login_ali_edu_test.js
@@ -1,17 +1,19 @@
 describe('login', function () {
 
-    it('login ali-edu', function() {
+    it('login ali-edu', async function() {
 
-        cy.visit("https://edu-cubelms.crscube.cn/")
+        cy.visit("https://edu-cubelms.3dshot.cn/")
 
         for(let i = 0; i < 1000; i++) {
             // input id, password
+            cy.wait(5000)
             cy.get('#lbInputText').type("dwlee@crscube.io")
             cy.get('#lbInputPwd2').type("cube1234")
 
             // click login button
             cy.get('.btn').click()
 
+            cy.wait(5000)
             // click myinfo
             cy.get('.myinfo').click()
 
@@ -23,3 +25,6 @@ describe('login', function () {
         }
     })
 })
+
+
+
