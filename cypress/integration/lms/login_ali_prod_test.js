0420afa4a7b58ae41817b1eabdf4e49643ca1c9a
Lee Dong Wook <sh95119@gmail.com>
2021-02-12 04:16:33 +0900

    Create login_ali_prod_test.js

---
+++ cypress/integration/lms/login_ali_prod_test.js
@@ -0,0 +1,27 @@
+describe('login', function () {
+
+    it('login ali-stg', function() {
+
+        cy.visit("https://cubelms.3dshot.cn/")
+
+        for(let i = 0; i < 1000; i++) {
+            // input id, password
+            cy.wait(5000)
+            cy.get('#lbInputText').type("dwlee@crscube.io")
+            cy.get('#lbInputPwd2').type("cube1234")
+
+            // click login button
+            cy.get('.btn').click()
+
+            cy.wait(5000)
+            // click myinfo
+            cy.get('.myinfo').click()
+
+            // click logout-btn
+            cy.get('.myinfo > .tooltip-box > .tooltip > .link > [href="#/DashBoard_Admin"]').click()
+
+            // click yes
+            cy.get('.btn-primary').click()
+        }
+    })
+})
